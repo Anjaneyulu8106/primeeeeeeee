2d536c198db335e8abd781944e8651b2a5af9be6
Anjaneyulu8106 <b.anjaneyulu2005@gmail.com>
2024-12-11 01:12:06 +0530

    Update prime.java

--- prime.java
+++ prime.java
@@ -3,7 +3,7 @@ public class  prime{
     public static void main(String[] args) {
         System.out.println("Prime numbers between 1 and 100 are:");
 
-        for (int num = 2; num <= 100; num++) { // Start from 2, as 1 is not prime
+        for (int num = 2; num < 100; num++) { // Start from 2, as 1 is not prime
             if (isPrime(num)) {
                 System.out.print(num + " ");
             }
